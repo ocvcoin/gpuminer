fa93951d9175857253736c61f7f2a6084ac8ec9e
ocvcoin <90601304+ocvcoin@users.noreply.github.com>
2023-08-06 12:04:22 +0000

    Add files via upload

---
+++ get_device_info.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+python "%cd%\pycl.py"
+
+echo Press any key to exit...
+pause >nul
---
+++ start_mining.cmd
@@ -0,0 +1,6 @@
+@echo off
+
+python "%cd%\ocvcoin_miner.py"
+
+echo Press any key to exit...
+pause >nul
