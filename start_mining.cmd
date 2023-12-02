8d0c0703d40b9a69d545e738973a3f9ca2dd809f
ocvcoin <90601304+ocvcoin@users.noreply.github.com>
2023-12-02 07:37:21 +0000

    Add files via upload

--- start_mining.cmd
+++ start_mining.cmd
@@ -1,6 +1,12 @@
 @echo off
 
-python "%cd%\ocvcoin_miner.py"
+
+SET mypath=%~dp0
+
+py -3.6 "%mypath%\ocvcoin_miner.py" || py -3 "%mypath%\ocvcoin_miner.py"
 
 echo Press any key to exit...
+
+
+
 pause >nul
